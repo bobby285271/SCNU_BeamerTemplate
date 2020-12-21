3c61fde196881bca884c780ab6f07609b598a73a
Bobby Rong <rjl931189261@126.com>
2020-12-21 15:05:09 +0800

    fix shebang

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
-!bin/bash
+#!/bin/bash
 
 xelatex SCNU_BeamerTemplate.tex
-xelatex SCNU_BeamerTemplate.tex
-xelatex SCNU_BeamerTemplate.tex
-xelatex SCNU_BeamerTemplate.tex
-
-
-rm SCNU_BeamerTemplate.aux SCNU_BeamerTemplate.log SCNU_BeamerTemplate.nav SCNU_BeamerTemplate.out SCNU_BeamerTemplate.snm SCNU_BeamerTemplate.toc
